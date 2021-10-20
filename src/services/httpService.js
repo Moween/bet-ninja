b6c5b773101a609e858997b7c93a21f8c69e5137
moween <moween1998@gmail.com>
2021-10-20 17:21:26 +0100

    Intercept errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
+axios.interceptors.response.use(null, (error) => {
+  const expectedError = error.response
+  && error.response.status >= 400
+  && error.response.status < 500;
+
+  if(!expectedError) {
+    // For Unexpected error
+    toast.error('An unexpected error occured!');
+  }
+  // For expected error
+  return Promise.reject(error);
+});
+
 const httpService = async (url) => {
   try {
     const response = await axios.get(url);
