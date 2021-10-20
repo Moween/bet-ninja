064bd7064ef8971f8501f92c51492f1f2705ff6f
moween <moween1998@gmail.com>
2021-10-20 17:00:44 +0100

    Overide mui default theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { theme } from '../utils/styles';
+import Header from './Header';
 import './App.css';
 
 function App() {
-  return null;
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+    </ThemeProvider>
+  )
 }
 
 export default App;
