ad953663a5cf10e58a177e530d7be6fbf2938d19
moween <moween1998@gmail.com>
2021-10-20 16:58:31 +0100

    Add header content

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
+import { useStyles } from '../utils/muiStyles';
+
+const Header = () => {
+  const { brandLogo, logoContainer, soccerIcon } = useStyles();
+  return (
+    <header className="header">
+      <Container maxWidth="lg">
+        <div className="header__div">
+          <div  className={logoContainer}>
+            <SportsSoccerIcon className={soccerIcon} />
+            <Typography 
+              className={brandLogo} 
+              variant="h4" component="h1" 
+            >
+              Bet
+              <Typography variant="h4" component="span" color="secondary">
+              Ninja
+              </Typography>
+            </Typography>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
