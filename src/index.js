6fc4219ad049bbccf5799c9e0378429b78970fa4
moween <moween1998@gmail.com>
2021-10-20 17:02:39 +0100

    Format code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
