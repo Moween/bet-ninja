ad24a151de97d2e416404ca9e0eea5294dbe26e5
moween <moween1998@gmail.com>
2021-10-20 18:09:26 +0100

    Add soccerData to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import soccerDataSlice from '../features/reducers/soccerDataSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    soccerData: soccerDataSlice,
+  },
 });
