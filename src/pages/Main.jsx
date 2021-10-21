aa3721ffc6c0991d3fd67f6d8afb33009b837df9
moween <moween1998@gmail.com>
2021-10-21 09:13:28 +0100

    Add boilerplate

---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useHistory, useDispatch } from 'react-router';
+
+const Home = () => {
+  return ( 
+    <main>
+      
+    </main>
+  );
+}
+ 
+export default Home;
