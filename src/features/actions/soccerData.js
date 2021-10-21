edf793b2f6f1c56b84d76a6a8697e6cbfd1cc4fd
moween <moween1998@gmail.com>
2021-10-21 09:18:15 +0100

    Fix linter double-quotes error.

--- src/features/actions/soccerData.js
+++ src/features/actions/soccerData.js
@@ -1,10 +1,11 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import httpService from "../../services/httpService";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import httpService from '../../services/httpService';
+import { apiUrl } from '../../api/config.json'
 
 // Async Logic using middleware
-const fetchSoccerData = createAsyncThunk("soccerData/fetchSoccerdata", 
+const fetchSoccerData = createAsyncThunk('soccerData/fetchSoccerdata', 
   async () => {
-    const response = await httpService();
+    const response = await httpService(apiUrl);
     return response.data;
   }
 );
