9527c591af119391cf11d0e479613fae68a7c3ef
moween <moween1998@gmail.com>
2021-10-21 09:47:52 +0100

    Fetch SoccerData on component mount.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import { useHistory, useDispatch } from 'react-router';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Container from '@material-ui/core/Container';
+import { ToastContainer } from 'react-toastify'; // To display network errors
+import fetchSoccerData from '../features/actions/soccerData';
+import { useStyles } from '../utils/muiStyles';
+import Table from '../components/Table';
 
 const Home = () => {
+  const { container } = useStyles();
+  const dispatch = useDispatch();
+  const soccerData = useSelector(state => state.soccerData.soccerData);
+  const soccerDataStatus  = useSelector(state => state.soccerData.status);
+
+  useEffect(() => {
+  if(soccerDataStatus === 'idle') {
+    dispatch(fetchSoccerData()); // Http Request to fetchData
+  }
+  }, [soccerDataStatus, dispatch]);
+
+  const renderContent = () => {
+    if(soccerDataStatus === 'loading') {
+      return <p style={{ marginTop: 10 }}>Loading...</p>
+    }else if(soccerDataStatus === 'succeeded') {
+      return (
+        <div>
+          {/* <Table soccerData={soccerData} /> */}
+        </div>
+      );
+    }
+  }
   return ( 
-    <main>
-      
+    <main>  
+      <ToastContainer />
+      <Container maxWidth="lg" className={container}>
+        {renderContent()}
+      </Container>  
     </main>
   );
 }
