cd3a47e6661375bc69ecf128b066f5f0066e4d3d
moween <moween1998@gmail.com>
2021-10-21 09:03:33 +0100

    Add  history page

---
+++ src/pages/History.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const History = () => {
+  const history = useHistory();
+  return ( 
+    <div>
+      History Page
+    </div>
+  );
+}
+ 
+export default History;
