f6661bb4fda75abcce6ff8a79d67ea140b0c64c3
moween <moween1998@gmail.com>
2021-10-22 18:17:16 +0100

    Add dataSlice to redux store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import dateSlice from '../reducers/dateSlice';
 import soccerDataSlice from '../reducers/soccerDataSlice';
 
 export const store = configureStore({
   reducer: {
     soccerData: soccerDataSlice,
+    date: dateSlice
   },
 });
