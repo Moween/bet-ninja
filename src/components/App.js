cdcbb2825980a4fdf7d60b0163f057ec57b73d2f
moween <moween1998@gmail.com>
2021-10-23 08:35:40 +0100

    Add side navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import { theme } from '../utils/muiStyles';
 import Home from '../pages/Main';
 import Header from './Header';
 import History from '../pages/History';
+import SideBar from '../components/SideBar'
 import '../css/App.css';
 
 function App() {
   return (    
     <ThemeProvider theme={theme}>
       <Header />
+      <SideBar />
       <Switch>
         <Route path='/pages/history' component={History} />
         <Route path='/' component={Home} />
