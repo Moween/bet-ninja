e243086e4c350ef78314ca62c407d635fac90473
moween <moween1998@gmail.com>
2021-10-24 09:40:14 +0100

    Use semantic element

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,49 +8,51 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
 const SideBar = () => {
-  const drawerWidth = '18%';
+  const drawerWidth = "18%";
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: {
+    <aside>
+      <Drawer
+        variant="permanent"
+        sx={{
           width: drawerWidth,
-          boxSizing: "border-box",
-          backgroundColor: "inherit",
-          color: "#ccc",
-          borderColor: "rgb(0, 0, 0, 0.30)",
-        },
-      }}
-    >
-      <Toolbar />
-      <Box sx={{ overflow: "hidden" }}>
-        <List>
-          {["HighLight", "Live", "Promo"].map((text, index) => (
-            <ListItem
-              button
-              key={text}
-              sx={{ paddingTop: 0, marginLeft: "1.05rem" }}
-            >
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />        
-        <List>
-          {["Soccer", "Winners", "Spam"].map((text, index) => (
-            <ListItem
-              button
-              key={text}
-              sx={{ paddingTop: 0, marginLeft: "1.05rem" }}
-            >
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Box>
-    </Drawer>
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "inherit",
+            color: "#ccc",
+            borderColor: "rgb(0, 0, 0, 0.30)",
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: "hidden" }}>
+          <List>
+            {["HighLight", "Live", "Promo"].map((text, index) => (
+              <ListItem
+                button
+                key={text}
+                sx={{ paddingTop: 0, marginLeft: "1.05rem" }}
+              >
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {["Soccer", "Winners", "Spam"].map((text, index) => (
+              <ListItem
+                button
+                key={text}
+                sx={{ paddingTop: 0, marginLeft: "1.05rem" }}
+              >
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
+    </aside>
   );
 };
 
