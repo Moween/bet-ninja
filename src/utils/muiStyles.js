dca759e3f9a134be94b0ec4a6377546a33cd0289
moween <moween1998@gmail.com>
2021-10-25 09:27:21 +0100

    Customize MuiLink Component

--- src/utils/muiStyles.js
+++ src/utils/muiStyles.js
@@ -1,5 +1,14 @@
+import React, { forwardRef } from 'react';
 import { createTheme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/styles';
+import { Link  } from 'react-router-dom';
+
+// Customize Mui Link Component
+const LinkBehavior = forwardRef((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return <Link data-testid="custom-link" ref={ref} to={href} {...other} />;
+});
 
 export const theme = createTheme({
   breakpoints: { // Media Query Breakpoint
@@ -16,7 +25,14 @@ export const theme = createTheme({
     secondary: {
       main: '#42b72a'
     }
-  }
+  },
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      },
+    },
+  },
 });
 
 export const useStyles = makeStyles((theme) => ({
