ed1bc54d9e157df5d2f426aab3d3759936432e6d
moween <moween1998@gmail.com>
2021-10-27 12:00:00 +0100

    Update main component
    Format Code
    Route to today and tomorrow components.
    Add class name to loading text.

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,47 +2,49 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Container from '@material-ui/core/Container';
 import { ToastContainer } from 'react-toastify'; // To display network errors
+import { Route, Switch } from 'react-router-dom';
 import fetchSoccerData from '../actions/soccerData';
 import { useStyles } from '../utils/muiStyles';
 import Today from '../components/Today';
 import Yesterday from '../components/Yesterday';
 import Tomorrow from '../components/Tomorrow';
 import Tabs from '../components/Tabs';
-import { Route } from 'react-router-dom';
 
 const Home = () => {
   const { container } = useStyles();
   const dispatch = useDispatch();
-  const soccerDataStatus  = useSelector(state => state.soccerData.status);
+  const soccerDataStatus = useSelector((state) => state.soccerData.status);
 
   useEffect(() => {
-  if(soccerDataStatus === 'idle') {
-    dispatch(fetchSoccerData()); // Http Request to fetchData
-  }
+    if (soccerDataStatus === 'idle') {
+      dispatch(fetchSoccerData()); // Http Request to fetchData
+    }
   }, [soccerDataStatus, dispatch]);
 
   const renderContent = () => {
-    if(soccerDataStatus === 'loading') {
-      return <p style={{ marginTop: 10 }}>Loading...</p>
-    }else if(soccerDataStatus === 'succeeded') {
+    if (soccerDataStatus === 'loading') {
+      return <p className="loadingText">Loading...</p>;
+    } else if (soccerDataStatus === 'succeeded') {
       return (
         <div>
-          <Route path="/yesterday" component={Yesterday} />
-          <Today />
-          <Tomorrow />
+          <Switch>
+            <Route path="/yesterday" component={Yesterday} />
+            <Route path="/tomorrow" component={Tomorrow} />
+            <Route path="/" component={Today} />
+          </Switch>
         </div>
       );
     }
-  }
-  return ( 
-    <main>  
+  };
+  return (
+    <main>
       <ToastContainer />
       <Container maxWidth="xl" className={container}>
         <Tabs />
         {renderContent()}
-      </Container>  
+      </Container>
     </main>
   );
-}
- 
+};
+
 export default Home;
