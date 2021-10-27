697d5df24618b810131ef4041f0f4933694098bd
moween <moween1998@gmail.com>
2021-10-27 09:18:17 +0100

    Use thunk for async logic
    Add thunk to fetch pastMatchesData

---
+++ src/actions/history.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import httpService from "../services/httpService";
+import { subDays, lightFormat } from "date-fns";
+
+async function fetchPastMatches() {
+  const pastThirtyDays = lightFormat(subDays(new Date(), 30), "yyyy-MM-dd"); // Get yesterday date in the format YYYY-MM-DD
+  const response = await httpService(
+    `${
+      process.env.REACT_APP_BASE_URL
+    }/tip?type=over2&from=${pastThirtyDays}&to=${lightFormat(new Date(), "yyyy-MM-dd")}
+    `
+  );
+  return response.data;
+}
+
+// Async Logic using middleware
+const fetchHistory = createAsyncThunk(
+  "soccerData/fetchHistory",
+  fetchPastMatches
+);
+
+export default fetchHistory;
