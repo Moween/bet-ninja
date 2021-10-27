f06672b2166e29d5a936bd0ea74abc7a6c54a341
moween <moween1998@gmail.com>
2021-10-27 09:26:21 +0100

    Add not available text and  format code

--- src/components/Tomorrow.jsx
+++ src/components/Tomorrow.jsx
@@ -1,31 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import parseJSON from 'date-fns/parseJSON';
 import addDays from 'date-fns/addDays';
+import Typography from '@material-ui/core/Typography';
 import Table from './Table';
 import { getDate } from '../utils/index';
 
 const Tomorrow = () => {
   let tomorrowDate = getDate(addDays(new Date(), 1));
   const [tomorrowMatches, setTomorrowMatches] = useState([]);
-  const soccerData = useSelector(state => state.soccerData.soccerData);
+  const soccerData = useSelector((state) => state.soccerData.soccerData);
 
   const filterTomorrowMatches = () => {
-    return  soccerData.filter(match => {
+    return soccerData.filter((match) => {
       const date = getDate(parseJSON(match.date));
       return date === tomorrowDate;
     });
-  }
+  };
 
   useEffect(() => {
-    const tomorrowMatches = filterTomorrowMatches; 
-    setTomorrowMatches(tomorrowMatches)
+    const tomorrowMatches = filterTomorrowMatches;
+    setTomorrowMatches(tomorrowMatches);
   }, []);
-  return ( 
+  return (
     <div>
-      <Table soccerData={tomorrowMatches} date={tomorrowDate}/>
+      {!soccerData.length === 0 ? (
+        <Table soccerData={tomorrowMatches} date={tomorrowDate} />
+      ) : (
+        <Typography variant="body1" align="center" component="body1">
+          Not available....
+        </Typography>
+      )}
     </div>
   );
-}
- 
+};
+
 export default Tomorrow;
