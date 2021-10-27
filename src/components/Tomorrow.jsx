a99d642826b49dd0e6699075764a2068c9b934de
moween <moween1998@gmail.com>
2021-10-27 06:47:35 +0100

    Update  Code
    Set state
    Filter out matches from redux store
    Pass in data as prop to Table Component

--- src/components/Tomorrow.jsx
+++ src/components/Tomorrow.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import parseJSON from 'date-fns/parseJSON';
+import addDays from 'date-fns/addDays';
 import Table from './Table';
+import { getDate } from '../utils/index';
 
 const Tomorrow = () => {
+  let tomorrowDate = getDate(addDays(new Date(), 1));
+  const [tomorrowMatches, setTomorrowMatches] = useState([]);
+  const soccerData = useSelector(state => state.soccerData.soccerData);
+
+  const filterTomorrowMatches = () => {
+    return  soccerData.filter(match => {
+      const date = getDate(parseJSON(match.date));
+      return date === tomorrowDate;
+    });
+  }
+
+  useEffect(() => {
+    const tomorrowMatches = filterTomorrowMatches; 
+    setTomorrowMatches(tomorrowMatches)
+  }, []);
   return ( 
-    <div id="tomorrow">
-      <p>Tomorrow</p>
-      <Table />
+    <div>
+      <Table soccerData={tomorrowMatches} date={tomorrowDate}/>
     </div>
   );
 }
--- src/components/Yesterday.jsx
+++ src/components/Yesterday.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux';
+import parseJSON from 'date-fns/parseJSON';
+import subDays from 'date-fns/subDays';
 import Table from './Table';
+import { getDate } from '../utils/index';
 
 const Yesterday = () => {
+  let yesterdayDate = getDate(subDays(new Date(), 1));
+  const [yesterdayMatches, setYesterdayMatches] = useState([]);
+  const soccerData = useSelector(state => state.soccerData.soccerData);
+
+  const filterYesterdayMatches = () => {
+    return  soccerData.filter(match => {
+      const date = getDate(parseJSON(match.date));
+      return date === yesterdayDate;
+    });
+  }
+
+  useEffect(() => {
+    const yesterdayMatches = filterYesterdayMatches; 
+    setYesterdayMatches(yesterdayMatches)
+  }, []);
   return ( 
     <div id="yesterday">
-      <Table />
+      <Table soccerData={yesterdayMatches} date={yesterdayDate} />
     </div>
   );
 }
