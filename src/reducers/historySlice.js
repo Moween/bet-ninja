677072978ce26ca3f4b8d3c392e5939f03d2efe7
moween <moween1998@gmail.com>
2021-10-27 07:17:14 +0100

    Handle thunk actions in extraReducers

--- src/reducers/historySlice.js
+++ src/reducers/historySlice.js
@@ -1,12 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
+import fetchHistory from "../actions/history";
 
 const historySlice =  createSlice({
   name: 'historySoccerData',
   initialState: {
     status: 'idle',
-    soccerData: [],
+    soccerData: '',
   },
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchHistory.pending, (state) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchHistory.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        // Adds any fetched data to the array
+        state.soccerData = state.soccerData.concat(action.payload);
+      })
+      .addCase(fetchHistory.rejected, (state) => {
+        state.status = 'failed'
+      })
+  }
 })
 
 export default historySlice.reducer;
