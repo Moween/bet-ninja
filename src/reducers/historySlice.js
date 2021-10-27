81545687ce17b8d4cce9beae0309c45bcdfa2047
moween <moween1998@gmail.com>
2021-10-27 11:53:38 +0100

    Add fetchDataStored action Format code

--- src/reducers/historySlice.js
+++ src/reducers/historySlice.js
@@ -1,28 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import fetchHistory from "../actions/history";
 
-const historySlice =  createSlice({
+const historySlice = createSlice({
   name: 'historySoccerData',
   initialState: {
     status: 'idle',
     soccerData: [],
   },
-  reducers: {},
+  reducers: {
+    fetchDataStored: (state, action) => {
+      state.soccerData = action.payload;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchHistory.pending, (state) => {
-        state.status = 'loading'
+        state.status = 'loading';
       })
       .addCase(fetchHistory.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        // Adds any fetched data to the array
+        // Adds any fetched data to the array using immer lib
         state.soccerData = state.soccerData.concat(action.payload);
       })
       .addCase(fetchHistory.rejected, (state) => {
         state.status = 'failed';
         state.soccerData = '';
-      })
-  }
-})
+      });
+  },
+});
 
+export const { fetchDataStored } = historySlice.actions;
 export default historySlice.reducer;
