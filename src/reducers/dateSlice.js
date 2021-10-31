7f396c1dc425e027082ad15abda8abc711760993
moween <moween1998@gmail.com>
2021-11-01 00:34:43 +0100

    Add thunk function and update date reducer

--- src/reducers/dateSlice.js
+++ src/reducers/dateSlice.js
@@ -1,16 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
+import fetch from '../actions/datePicker';
 
 const dateSlice = createSlice({
   name: 'date',
   initialState: {
-    date: new Date(),
+    startDate: new Date(),
+    endDate: new Date(),
+    status: 'idle',
+    soccerData: [],
   },
   reducers: {
-    datePicked: (state, action) => {
-      state.date = action.payload;
+    startDatePicked: (state, action) => {
+      state.startDate = action.payload;
+    },
+    endDatePicked: (state, action) => {
+      state.endDate = action.payload;
     }
+  },
+  extraReducers(builder) {
+    builder
+    .addCase(fetch.pending, (state) => {
+      state.status = 'loading';
+    })
+    .addCase(fetch.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.soccerData = state.soccerData.concat(action.payload);
+    })
+    .addCase(fetch.rejected, (state) => {
+      state.status = 'failed';
+      state.soccerData = '';
+    })
+
   }
 });
 
-export const { datePicked } = dateSlice.actions;
+export const { startDatePicked, endDatePicked } = dateSlice.actions;
 export default dateSlice.reducer;
