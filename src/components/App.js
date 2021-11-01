c09e22a7c413409b1fc3effd02ac7ce565e84ab1
moween <moween1998@gmail.com>
2021-11-01 01:06:40 +0100

    Delete sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,12 @@ import { theme } from '../utils/muiStyles';
 import Home from '../pages/Main';
 import Header from './Header';
 import History from '../pages/History';
-import SideBar from '../components/SideBar';
 import '../css/App.css';
 
 function App() {
   return (    
     <ThemeProvider theme={theme}>
       <Header />
-      <SideBar />
       <Switch>
         <Route path='/pages/history' component={History} />
         <Route path='/' component={Home} />
