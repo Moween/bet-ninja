991fd199e53257e7b1c48edb8d4c2cec0e54b30f
moween <moween1998@gmail.com>
2021-11-03 13:48:32 +0100

    Update Code
    Prettify Code.
    Pass in right argument.

--- src/actions/history.js
+++ src/actions/history.js
@@ -1,16 +1,18 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import httpService from "../services/httpService";
-import { subDays, lightFormat } from "date-fns";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import httpService from '../services/httpService';
+import { subDays, lightFormat } from 'date-fns';
 
 async function fetchPastMatches() {
-  const pastThirtyDays = lightFormat(subDays(new Date(), 30), "yyyy-MM-dd"); // Get yesterday date in the format YYYY-MM-DD
-  const aDayBeforeCurrent = lightFormat(subDays(new Date(), 1), "yyyy-MM-dd");
-  const response = await httpService(pastThirtyDays, aDayBeforeCurrent);
+  const pastThirtyDays = lightFormat(subDays(new Date(), 30), 'yyyy-MM-dd'); // Get yesterday date in the format YYYY-MM-DD
+  const aDayBeforeCurrent = lightFormat(subDays(new Date(), 1), 'yyyy-MM-dd');
+  const response = await httpService(
+    `${process.env.REACT_APP_BASE_URL}/tip?type=over2&from=${pastThirtyDays}&to=${aDayBeforeCurrent}`
+  );
   return response.data;
 }
 // Async Logic using middleware
 const fetchHistory = createAsyncThunk(
-  "soccerData/fetchHistory",
+  'soccerData/fetchHistory',
   fetchPastMatches
 );
 
