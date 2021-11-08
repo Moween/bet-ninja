c1966b431af273ac67f22ebdec561e7ac64c2b23
moween <moween1998@gmail.com>
2021-11-08 08:51:38 +0100

    Override muiDatePicker default styles

--- src/utils/muiStyles.js
+++ src/utils/muiStyles.js
@@ -33,7 +33,29 @@ export const theme = createTheme({
         component: LinkBehavior,
       },
     },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          color: '#ccc',
+        },
+      }
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: '#ccc',
+        },
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: '#ccc'
+        },      
+      }
+    }
   },
+  
 });
 
 export const useStyles = makeStyles((theme) => ({
