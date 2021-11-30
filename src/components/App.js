12e194d684d3b285ffa513e2043010fe53ed8642
moween <moween1998@gmail.com>
2021-11-30 18:46:19 +0100

    Dispatch mediaQuery actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,35 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { useDispatch } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { theme } from '../utils/muiStyles';
+import { useMediaQuery } from '@material-ui/core';
+
 import Header from './Header';
 import Home from '../pages/Main';
 import History from '../pages/History';
 import Footer from './Footer';
 import '../css/App.css';
-import { useMediaQuery } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
 import { setSmallScreen, setMediumScreen } from '../reducers/mediaQuery';
 
 function App() {
+  const dispatch = useDispatch();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const isTablet = useMediaQuery(theme.breakpoints.down('lg'));
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(setSmallScreen(isMobile));
-  }, [dispatch, isMobile]);
-  useEffect(() => {
-    dispatch(setMediumScreen(isTablet));
-  }, [dispatch, isTablet]);
 
-  return ( 
+  dispatch(setMediumScreen(isTablet));
+  dispatch(setSmallScreen(isMobile));
+
+  return (
     <ThemeProvider theme={theme}>
       <Header />
       <Switch>
-        <Route path='/pages/history' component={History} />
-        <Route path='/' component={Home} />
+        <Route path="/pages/history" component={History} />
+        <Route path="/" component={Home} />
       </Switch>
       <Footer />
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
