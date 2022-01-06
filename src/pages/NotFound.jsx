1681a2ce4b15ab00aa02a95893b854a1a23609c5
moween <moween1998@gmail.com>
2022-01-06 11:43:06 +0100

    Update import with the correct path

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Box from '@material-ui/core/core/Box';
-import Typography from '@material-ui/core/core/Box';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Box';
 
 const NotFound = () => {
   return (
