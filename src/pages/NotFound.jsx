4dd8d64b5da2a0ccfe41e35b1dd505957eaa1b04
moween <moween1998@gmail.com>
2022-01-06 10:50:10 +0100

    Add Notfound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Box from '@material-ui/core/core/Box';
+import Typography from '@material-ui/core/core/Box';
+
+const NotFound = () => {
+  return (
+    <Box>
+     <Typography>Page Not Found</Typography> 
+    </Box>
+  )
+}
+
+export default NotFound;
